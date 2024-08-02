80ef7a6520e0e8740a12820493cc8ff026244aca
Ross Gordon <rossagordonstl@gmail.com>
2024-08-01 23:11:28 -0400

    Fix className ts error

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>Fix It MVP</Text>
       <StatusBar style='auto' />
       <Link href='/profile' style={{ color: "blue" }}>
@@ -15,11 +15,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
