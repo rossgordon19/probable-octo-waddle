2b4c5b6ac8264362684ad8cd1e0c3d54fda3a191
Ross Gordon <rossagordonstl@gmail.com>
2024-08-01 23:06:59 -0400

    Set up app. Install dependencies

---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from "react-native";
+
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Fix It MVP</Text>
+      <StatusBar style='auto' />
+      <Link href='/profile' style={{ color: "blue" }}>
+        Go to Profile
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
