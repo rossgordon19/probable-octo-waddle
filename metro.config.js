6b634944a1db4b7ec3999c5599c9e19fe6b79138
Ross Gordon <rossagordonstl@gmail.com>
2024-08-10 19:07:21 -0400

    update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,4 @@ const { getDefaultConfig } = require("expo/metro-config");
 const defaultConfig = getDefaultConfig(__dirname);
 const Dotenv = require("dotenv");
 
-// Load .env file contents into process.env
-Dotenv.config();
-
 module.exports = defaultConfig;
