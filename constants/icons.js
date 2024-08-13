af35295d4a2c386e34147f490bc88d0358eb3d48
Ross Gordon <rossagordonstl@gmail.com>
2024-08-12 22:01:48 -0400

    Project clean up

--- constants/icons.js
+++ constants/icons.js
@@ -5,18 +5,13 @@ import eyeHide from "../assets/icons/eye-hide.png";
 import hammer from "../assets/icons/hammer.png";
 import home from "../assets/icons/home.png";
 import leftArrow from "../assets/icons/left-arrow.png";
-import logout from "../assets/icons/logout.png";
-import menu from "../assets/icons/menu.png";
-import play from "../assets/icons/play.png";
 import plus from "../assets/icons/plus.png";
 import profile from "../assets/icons/profile.png";
 import rightArrow from "../assets/icons/right-arrow.png";
 import search from "../assets/icons/search.png";
 import sparkle from "../assets/icons/sparkle.png";
-import upload from "../assets/icons/upload.png";
 
 export default {
-  play,
   bookmark,
   camera,
   hammer,
@@ -24,11 +19,8 @@ export default {
   plus,
   profile,
   leftArrow,
-  menu,
   search,
-  upload,
   rightArrow,
-  logout,
   eyeHide,
   eye,
   sparkle
